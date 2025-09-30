d502e0f6d4cfad230b0336a671a48665f5e9400a
bharcode22 <bharcode22@gmail.com>
2025-09-30 16:34:51 +0800

    delete postgre listener

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RabbitmqListenerModule } from './rabbitmq-listener/rabbitmq-listener.module';
 import { DockerEventListenerModule } from './docker-event-listener/docker-event-listener.module';
-import { PostgresListenerService } from './postgre-listener/postgre-listener.service';
 
 @Module({
   imports: [
@@ -15,7 +14,6 @@ import { PostgresListenerService } from './postgre-listener/postgre-listener.ser
   ],
   providers: [
     AppService, 
-    PostgresListenerService
   ],
 })
 export class AppModule {}
